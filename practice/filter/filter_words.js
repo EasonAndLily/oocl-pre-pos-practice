0e6b2b79e2bde90bacd3c9666e1bd43465bd33f8
Eason Li <lijinghua@odc.cmbchina.cn>
2019-07-09 23:31:29 +0800

    set show case evn

--- practice/filter/filter_words.js
+++ practice/filter/filter_words.js
@@ -1,7 +1,26 @@
 "use strict";
 
-function filterWordsByLength(words, length) {
-  return null;
+function filterWordsMoreThanSix(words) {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 6) {
+      result.push(words[i]);
+    }
+  }
+  return result;
 }
 
-module.exports = filterWordsByLength;
+function filterWordsMoreThanFour(words) {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      result.push(words[i]);
+    }
+  }
+  return result;
+}
+
+module.exports = {
+  filterWordsMoreThanSix: filterWordsMoreThanSix,
+  filterWordsMoreThanFour: filterWordsMoreThanFour
+};
--- specs/filter/filter_words_spec.js
+++ specs/filter/filter_words_spec.js
@@ -1,20 +1,19 @@
 "use strict";
 
-var filterWordsByLength = require("../../practice/filter/filter_words.js");
+const filterWords = require("../../practice/filter/filter_words.js");
 
 describe("choose_words", function() {
-  let words = [
-    "spray",
-    "limit",
-    "elite",
-    "exuberant",
-    "destruction",
-    "present"
-  ];
+  let words = ["spra", "limit", "elit", "exuberant", "destruction", "present"];
 
   it("choose words length more than six", function() {
-    var result = filterWordsByLength(words, 6);
+    var result = filterWords.filterWordsMoreThanSix(words, 6);
 
     expect(result).toEqual(["exuberant", "destruction", "present"]);
+  });
+
+  it("choose words length more than four", function() {
+    var result = filterWords.filterWordsMoreThanFour(words, 4);
+
+    expect(result).toEqual(["limit", "exuberant", "destruction", "present"]);
   });
 });
