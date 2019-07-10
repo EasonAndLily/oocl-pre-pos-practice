fa7cf6c1fbd0f04b5b15483b2d20f0ce7cd715d2
Eason Li <lijinghua@odc.cmbchina.cn>
2019-07-10 11:56:07 +0800

    Refactor: use ES6 filter to replace filter words method

--- practice/filter/filter_words.js
+++ practice/filter/filter_words.js
@@ -1,18 +1,8 @@
 "use strict";
 
 function filterWordsByCondition(words, condition) {
-  return words.filterWords(condition);
+  return words.filter(condition);
 }
-
-Array.prototype.filterWords = function(condition) {
-  let result = [];
-  for (let i = 0; i < this.length; i++) {
-    if (condition(this[i])) {
-      result.push(this[i]);
-    }
-  }
-  return result;
-};
 
 module.exports = {
   filterWordsByCondition: filterWordsByCondition
