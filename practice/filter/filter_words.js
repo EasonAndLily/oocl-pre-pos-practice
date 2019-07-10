ba78fab3c454ab728d9980028ffd03d3ea34272f
Eason Li <lijinghua@odc.cmbchina.cn>
2019-07-10 11:50:37 +0800

    Refactor: throw the condition to Array prototype

--- practice/filter/filter_words.js
+++ practice/filter/filter_words.js
@@ -1,14 +1,18 @@
 "use strict";
 
 function filterWordsByCondition(words, condition) {
+  return words.filterWords(condition);
+}
+
+Array.prototype.filterWords = function(condition) {
   let result = [];
-  for (let i = 0; i < words.length; i++) {
-    if (condition(words[i])) {
-      result.push(words[i]);
+  for (let i = 0; i < this.length; i++) {
+    if (condition(this[i])) {
+      result.push(this[i]);
     }
   }
   return result;
-}
+};
 
 module.exports = {
   filterWordsByCondition: filterWordsByCondition
