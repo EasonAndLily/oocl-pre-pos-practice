f8ca2ec53a8037d5abd0705d32bed9183a4bc4dd
Eason Li <lijinghua@odc.cmbchina.cn>
2019-07-10 11:33:54 +0800

    Refactor: extra condition as params

--- practice/filter/filter_words.js
+++ practice/filter/filter_words.js
@@ -1,19 +1,19 @@
 "use strict";
 
-function filterWordsMoreThanSix(words) {
+function filterWordsMoreThanSix(words, condition) {
   let result = [];
   for (let i = 0; i < words.length; i++) {
-    if (words[i].length > 6) {
+    if (condition(words[i])) {
       result.push(words[i]);
     }
   }
   return result;
 }
 
-function filterWordsMoreThanFour(words) {
+function filterWordsMoreThanFour(words, condition) {
   let result = [];
   for (let i = 0; i < words.length; i++) {
-    if (words[i].length > 4) {
+    if (condition(words[i])) {
       result.push(words[i]);
     }
   }
--- specs/filter/filter_words_spec.js
+++ specs/filter/filter_words_spec.js
@@ -4,15 +4,20 @@ const filterWords = require("../../practice/filter/filter_words.js");
 
 describe("choose_words", function() {
   let words = ["spra", "limit", "elit", "exuberant", "destruction", "present"];
-
   it("choose words length more than six", function() {
-    const result = filterWords.filterWordsMoreThanSix(words);
+    const condition = (word) => {
+      return word.length > 6;
+    };
+    const result = filterWords.filterWordsMoreThanSix(words, condition);
 
     expect(result).toEqual(["exuberant", "destruction", "present"]);
   });
 
   it("choose words length more than four", function() {
-    const result = filterWords.filterWordsMoreThanFour(words);
+    const condition = (word) => {
+      return word.length > 4;
+    };
+    const result = filterWords.filterWordsMoreThanFour(words, condition);
 
     expect(result).toEqual(["limit", "exuberant", "destruction", "present"]);
   });
